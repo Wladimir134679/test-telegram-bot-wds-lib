afa2e267541de007b59f41a15cec53b518e956d4
WDeath <wladimir134679@gmail.com>
2022-06-19 16:12:19 +0300

    Создание сессия для чатов и внутри них для юзеров

--- src/main/java/ru/wdeath/testbotspring/TestCommandBot.java
+++ src/main/java/ru/wdeath/testbotspring/TestCommandBot.java
@@ -8,16 +8,34 @@ import ru.wdeath.managerbot.lib.bot.TelegramLongPollingEngine;
 import ru.wdeath.managerbot.lib.bot.annotations.ParamName;
 import ru.wdeath.managerbot.lib.bot.annotations.TextCommandFirst;
 import ru.wdeath.managerbot.lib.bot.annotations.TextCommandNames;
+import ru.wdeath.managerbot.lib.bot.annotations.TextCommandOther;
+import ru.wdeath.managerbot.lib.bot.session.UserBotSession;
+
+import java.util.UUID;
 
 @TextCommandNames("/test")
 @Service
 public class TestCommandBot {
 
     @TextCommandFirst
-    public void test(TelegramLongPollingEngine engine, Message message, @ParamName("chatId") Long chatId){
+    public void test(TelegramLongPollingEngine engine, Message message, @ParamName("chatId") Long chatId, UserBotSession userBotSession){
         var send = new SendMessage();
         send.setChatId(String.valueOf(chatId));
+        userBotSession.setData("Tut data: " + UUID.randomUUID().toString());
         send.setText("тест пройден)");
+
+        try {
+            engine.execute(send);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @TextCommandOther
+    public void other(TelegramLongPollingEngine engine, Message message, UserBotSession userBotSession, @ParamName("chatId") Long chatId){
+        var send = new SendMessage();
+        send.setChatId(String.valueOf(chatId));
+        send.setText("Были такие данные: " + userBotSession.getData());
 
         try {
             engine.execute(send);
