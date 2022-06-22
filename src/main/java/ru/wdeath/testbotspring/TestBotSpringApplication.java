f16eb2da52ef27f37a2176506d4655f65357dcfa
WDeath <wladimir134679@gmail.com>
2022-06-22 16:43:53 +0300

    Исправление и запуск правильный JPA в стартере

--- src/main/java/ru/wdeath/testbotspring/TestBotSpringApplication.java
+++ src/main/java/ru/wdeath/testbotspring/TestBotSpringApplication.java
@@ -1,18 +1,17 @@
 package ru.wdeath.testbotspring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.wdeath.managerbot.lib.WdsManagerBotLibApplication;
-import ru.wdeath.managerbot.lib.bot.TelegramBotApiStarterService;
-
-import javax.annotation.PostConstruct;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.wdeath.managerbot.lib.TelegramBotAutoConfig;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = {TelegramBotAutoConfig.class})
+@EnableCaching
 public class TestBotSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestBotSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestBotSpringApplication.class, args);
+    }
 }
