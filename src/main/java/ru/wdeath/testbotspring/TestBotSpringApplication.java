cfd80b8bd23274901268f38a8d40278f3517f215
wdeath <wladimir134679@gmail.com>
2022-10-26 19:08:16 +0300

    create module program agent

--- src/main/java/ru/wdeath/testbotspring/TestBotSpringApplication.java
+++ src/main/java/ru/wdeath/testbotspring/TestBotSpringApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import ru.wdeath.managerbot.lib.TelegramBotAutoConfig;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = {TelegramBotAutoConfig.class})
 @EnableCaching
 public class TestBotSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestBotSpringApplication.class, args);
     }
+
+
 }
--- src/main/java/ru/wdeath/testbotspring/web/SecurityConfig.java
+++ src/main/java/ru/wdeath/testbotspring/web/SecurityConfig.java
@@ -5,21 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import ru.wdeath.managerbot.lib.web.config.SecurityTemplate;
-import ru.wdeath.managerbot.lib.web.config.security.ErrorEndpoint;
-import ru.wdeath.managerbot.lib.web.config.security.TokenFilter;
+import ru.wdeath.programagent.lib.web.config.SecurityTemplate;
+import ru.wdeath.programagent.lib.web.config.security.ErrorEndpoint;
+import ru.wdeath.programagent.lib.web.config.security.TokenFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final TokenFilter tokenFilter;
-    private final ErrorEndpoint errorEndpoint;
     private final SecurityTemplate template;
 
     public SecurityConfig(TokenFilter tokenFilter, ErrorEndpoint errorEndpoint) {
-        this.tokenFilter = tokenFilter;
-        this.errorEndpoint = errorEndpoint;
         this.template = new SecurityTemplate(errorEndpoint, tokenFilter);
     }
 
