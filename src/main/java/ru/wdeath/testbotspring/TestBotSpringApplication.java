c75de7a5a74907cf47eee7d82a43758580299882
wdeath <wladimir134679@gmail.com>
2022-11-03 19:34:01 +0300

    add chart system / return preview and get data

--- src/main/java/ru/wdeath/testbotspring/TestBotSpringApplication.java
+++ src/main/java/ru/wdeath/testbotspring/TestBotSpringApplication.java
@@ -1,10 +1,14 @@
 package ru.wdeath.testbotspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.wdeath.managerbot.lib.TelegramBotAutoConfig;
+import ru.wdeath.managerbot.lib.charts.ChartTimestampMessage;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableCaching
@@ -13,6 +17,5 @@ public class TestBotSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestBotSpringApplication.class, args);
     }
-
 
 }
--- src/test/java/ru/wdeath/testbotspring/TestBotSpringApplicationTests.java
+++ src/test/java/ru/wdeath/testbotspring/TestBotSpringApplicationTests.java
@@ -1,13 +1,23 @@
 package ru.wdeath.testbotspring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.wdeath.managerbot.lib.charts.ChartTimestampMessage;
+import ru.wdeath.programagent.lib.model.ChartTimestampModel;
+
+import java.util.List;
 
 @SpringBootTest
 class TestBotSpringApplicationTests {
 
+	@Autowired
+	private ChartTimestampMessage chartTimestampMessage;
+
 	@Test
 	void contextLoads() {
+		List<ChartTimestampModel.ChartPointModel> generate = chartTimestampMessage.generate(chartTimestampMessage.getDefaultFromTime(), chartTimestampMessage.getDefaultToTime());
+		System.out.println(generate);
 	}
 
 }
