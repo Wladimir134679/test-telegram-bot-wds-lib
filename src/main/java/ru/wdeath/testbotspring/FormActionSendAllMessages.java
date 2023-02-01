8f711349b5349312199d4f7a69121598565d6af7
wdeath <wladimir134679@gmail.com>
2023-02-01 21:01:03 +0300

    create form action model and handler

---
+++ src/main/java/ru/wdeath/testbotspring/FormActionSendAllMessages.java
@@ -0,0 +1,55 @@
+package ru.wdeath.testbotspring;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.wdeath.programagent.lib.form.actions.FormActionHandler;
+import ru.wdeath.programagent.lib.service.FormActionService;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class FormActionSendAllMessages implements FormActionHandler {
+
+    private final FormActionService formActionService;
+    private String lastSend = "";
+
+    @Override
+    public String getTitle() {
+        return "Рассылка сообщений";
+    }
+
+    @Override
+    public String getName() {
+        return "send-message";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Отправка всем сообщений";
+    }
+
+    @Override
+    public String getType() {
+        return "message";
+    }
+
+    @Override
+    public String getText() {
+        return lastSend;
+    }
+
+    @Override
+    public void processingText(String text) {
+        log.info("Processing new message: " + text);
+        lastSend = text;
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("Start init form action");
+        formActionService.registration(this);
+    }
+}
