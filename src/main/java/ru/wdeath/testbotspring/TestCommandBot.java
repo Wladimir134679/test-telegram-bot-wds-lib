74afa924c35e04aea4f1df2b73e94d462c1cde52
wdeath <wladimir134679@gmail.com>
2024-01-27 17:47:01 +0300

    create send notification in ExecutorService

--- src/main/java/ru/wdeath/testbotspring/TestCommandBot.java
+++ src/main/java/ru/wdeath/testbotspring/TestCommandBot.java
@@ -1,9 +1,12 @@
 package ru.wdeath.testbotspring;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.wdeath.programagent.lib.model.NewNotificationModel;
+import ru.wdeath.programagent.lib.service.BackClientService;
 import ru.wdeath.telegram.bot.starter.TelegramLongPollingEngine;
 import ru.wdeath.telegram.bot.starter.annotations.CommandFirst;
 import ru.wdeath.telegram.bot.starter.annotations.CommandNames;
@@ -11,18 +14,27 @@ import ru.wdeath.telegram.bot.starter.annotations.CommandOther;
 import ru.wdeath.telegram.bot.starter.annotations.ParamName;
 import ru.wdeath.telegram.bot.starter.session.UserBotSession;
 
+import java.util.Random;
 import java.util.UUID;
 
 @CommandNames("/test")
 @Service
+@RequiredArgsConstructor
 public class TestCommandBot {
 
+    private final BackClientService backClientService;
+
     @CommandFirst
-    public void test(TelegramLongPollingEngine engine, Message message, @ParamName("chatId") Long chatId, UserBotSession userBotSession){
+    public void test(TelegramLongPollingEngine engine, Message message, @ParamName("chatId") Long chatId, UserBotSession userBotSession) {
         var send = new SendMessage();
         send.setChatId(String.valueOf(chatId));
         userBotSession.setData("Tut data: " + UUID.randomUUID().toString());
         send.setText("тест пройден)");
+
+        backClientService.newNotification(
+                "Тест пользователем " + message.getFrom().getFirstName() + " пройден",
+                "Результат теста " + (int) (Math.random() * 100),
+                NewNotificationModel.Priority.HIGH);
 
         try {
             engine.execute(send);
@@ -32,7 +44,7 @@ public class TestCommandBot {
     }
 
     @CommandOther
-    public void other(TelegramLongPollingEngine engine, Message message, UserBotSession userBotSession, @ParamName("chatId") Long chatId){
+    public void other(TelegramLongPollingEngine engine, Message message, UserBotSession userBotSession, @ParamName("chatId") Long chatId) {
         var send = new SendMessage();
         send.setChatId(String.valueOf(chatId));
         send.setText("Были такие данные: " + userBotSession.getData());
